9550ecc1eba51fe2274d0e333a886a86fd6ea69e
Ksu Ksu <ksenia685@gmail.com>
2016-11-14 19:10:32 +0200

    message commit

---
+++ javacore/src/main/java/com/kseniya/app/SampleController.java
@@ -0,0 +1,13 @@
+package com.kseniya.app;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+	@RequestMapping("sample")
+	public String helloSpring(){
+		return "Hello Spring";
+	}
+}
